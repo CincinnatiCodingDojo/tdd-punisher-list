12d81f47d569f71b4f34b473dc509e425830e55e
dcpdev <developer@kroger.com>
2016-04-03 22:55:08 -0400

    DCPARTA-1423 | Jim, Tim | Add propTypes validation to store locator component

--- src/components/store_locator.js
+++ src/components/store_locator.js
@@ -1,14 +1,28 @@
-export default (React) => ({ store, findStore }) => {
-  let myTextInput = null;
-  const handleClick = () => findStore(myTextInput.value);
+export default (React) => {
+  const { shape, string, func } = React.PropTypes;
 
-  return (
-    <div>
-      <input type="text" defaultValue="01400301" ref={(ref) => { myTextInput = ref; }}/>
-      <button type="submit" onClick={handleClick}>Find</button>
-      <hr />
-      <div>{store.brand}</div>
-      <div>{store.addressLineOne}</div>
-    </div>
-  );
+  const Component = ({ store, findStore }) => {
+    let myTextInput = null;
+    const handleClick = () => findStore(myTextInput.value);
+
+    return (
+      <div>
+        <input type="text" defaultValue="01400301" ref={(ref) => { myTextInput = ref; }}/>
+        <button type="submit" onClick={handleClick}>Find</button>
+        <hr />
+        <div>{store.brand}</div>
+        <div>{store.addressLineOne}</div>
+      </div>
+    );
+  };
+
+  Component.propTypes = {
+    store: shape({
+      brand: string,
+      addressLineOne: string
+    }),
+    findStore: func
+  };
+
+  return Component;
 };
