705e1afa80168c94ab5461f96bca6d0c110d58ee
Jim Anders <jim@janders223.com>
2016-04-15 15:51:52 -0400

    DCPARTA-1425 | Jim | Move CSS import from node_modules to CSS file

--- src/containers/store_locator.container.js
+++ src/containers/store_locator.container.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import { locateStore, inputValChange } from '../actions/find_store';
 import createStoreLocator from '../components/store_locator';
-import 'react-kuic/lib/kuic.css';
 import '../style/index.css';
 
 export default (React) => {
